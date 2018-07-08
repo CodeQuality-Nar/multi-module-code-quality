d5140e915dfdfa557e51acf832cea4c5030300ac
Narendra <nknirala.0071@gmail.com>
2018-07-09 00:19:25 +0530

    Checkstyle Working fine till this commit.

---
+++ driver/src/main/java/com/java/driver/Driver.java
@@ -0,0 +1,13 @@
+package com.java.driver;
+
+import com.java.Department;
+import com.java.Employee;
+
+public class Driver {
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+        Department dept = new Department();
+        //Employee
+        //List
+    }
+}
---
+++ module1/src/main/java/com/java/Module1Runner.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import java.util.List;
+
+public class Module1Runner {
+    public void method1(){
+        Employee emp = new Employee();
+        emp.setEmpName("nare");
+        System.out.println(emp.getEmpName());
+
+        //List
+    }
+}
