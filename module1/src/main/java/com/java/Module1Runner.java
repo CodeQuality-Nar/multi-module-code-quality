2731c083fe625925edebe909eef703f8917136f8
Narendra <nknirala.0071@gmail.com>
2018-07-16 23:29:07 +0530

    Pmd plugin configured, checkstyle errors suppressed.

--- module1/src/main/java/com/java/Module1Runner.java
+++ module1/src/main/java/com/java/Module1Runner.java
@@ -1,9 +1,9 @@
 package com.java;
 
-import java.util.List;
+//import java.util.List;
 
 public class Module1Runner {
-    public void method1(){
+    public void method1() {
         Employee emp = new Employee();
         emp.setEmpName("nare");
         System.out.println(emp.getEmpName());
